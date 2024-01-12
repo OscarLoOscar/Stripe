d7a4d8082cb978d1d6eb03b9e4d1b50ff54d2238
OscarLoOscar <lokongkitoscar@gmail.com>
2024-01-12 17:41:07 +0800

    Stripe Subscription@12-1-2024

--- shoppingcart/src/main/java/com/example/shoppingcart/exception/handler/GlobalExceptionHandler.java
+++ shoppingcart/src/main/java/com/example/shoppingcart/exception/handler/GlobalExceptionHandler.java
@@ -1,25 +1,18 @@
 package com.example.shoppingcart.exception.handler;
 
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.example.shoppingcart.exception.BusinessException;
+import com.example.shoppingcart.exception.ProductNotExistException;
+import com.example.shoppingcart.exception.UserNotExistException;
 import com.example.shoppingcart.exception.setting.ApiResp;
 import com.example.shoppingcart.exception.setting.Code;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-  // @ExceptionHandler(value = FSSE2309Exception.class)
-  // @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-  // public ApiResp<Void> finnhubExceptionHandler(FSSE2309Exception e) {
-  //   return ApiResp.<Void>builder() //
-  //       .status(Code.fromCode(e.getCode())) //
-  //       .concatMessageIfPresent(e.getMessage())//
-  //       // .data(null) //
-  //       .build();
-  // }
 
   @ExceptionHandler(value = BusinessException.class)
   @ResponseStatus(value = HttpStatus.BAD_REQUEST)
@@ -31,35 +24,35 @@ public class GlobalExceptionHandler {
         .build();
   }
 
-  // @ExceptionHandler(value = UserNotExistException.class)
-  // @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-  // public ApiResp<Void> finnhubExceptionHandler(UserNotExistException e) {
-  //   return ApiResp.<Void>builder() //
-  //       .status(Code.fromCode(e.getCode())) //
-  //       .concatMessageIfPresent(e.getMessage())//
-  //       // .data(null) //
-  //       .build();
-  // }
+  @ExceptionHandler(value = UserNotExistException.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public ApiResp<Void> finnhubExceptionHandler(UserNotExistException e) {
+    return ApiResp.<Void>builder() //
+        .status(Code.fromCode(e.getCode())) //
+        .concatMessageIfPresent(e.getMessage())//
+        // .data(null) //
+        .build();
+  }
 
-  // @ExceptionHandler(value = ProductNotExistException.class)
-  // @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-  // public ApiResp<Void> finnhubExceptionHandler(ProductNotExistException e) {
-  //   return ApiResp.<Void>builder() //
-  //       .status(Code.fromCode(e.getCode())) //
-  //       .concatMessageIfPresent(e.getMessage())//
-  //       // .data(null) //
-  //       .build();
-  // }
+  @ExceptionHandler(value = ProductNotExistException.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public ApiResp<Void> finnhubExceptionHandler(ProductNotExistException e) {
+    return ApiResp.<Void>builder() //
+        .status(Code.fromCode(e.getCode())) //
+        .concatMessageIfPresent(e.getMessage())//
+        // .data(null) //
+        .build();
+  }
 
-  // @ExceptionHandler(value = NoSuchElementException.class)
-  // @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-  // public ApiResp<Void> finnhubExceptionHandler(NoSuchElementException e) {
-  //   return ApiResp.<Void>builder() //
-  //       .status(getRespCode(e))//
-  //       .concatMessageIfPresent(e.getMessage())//
-  //       // .data(null) //
-  //       .build();
-  // }
+  @ExceptionHandler(value = NoSuchElementException.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+  public ApiResp<Void> finnhubExceptionHandler(NoSuchElementException e) {
+    return ApiResp.<Void>builder() //
+        .status(getRespCode(e))//
+        .concatMessageIfPresent(e.getMessage())//
+        // .data(null) //
+        .build();
+  }
 
   @ExceptionHandler(value = RuntimeException.class)
   @ResponseStatus(value = HttpStatus.BAD_REQUEST)
@@ -86,6 +79,6 @@ public class GlobalExceptionHandler {
       return Code.IAE_EXCEPTION;
     }
     // ...
-    return null;
+    return Code.NOT_FOUND;
   }
 }
